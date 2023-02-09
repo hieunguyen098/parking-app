39550f31bb2e02aa57030beff99c24cba0f39dc1
Binh Cao <binh17122001@gmail.com>
2023-02-09 20:26:49 +0700

    setup mockapi

--- OtherDevices/screens/NumberPlateScanner/NumberPlateScanner.tsx
+++ OtherDevices/screens/NumberPlateScanner/NumberPlateScanner.tsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
 
 const NumberPlateScanner = () => {
-  return (
-    <View>
-      <Text>NumberPlateScanner</Text>
-    </View>
-  )
-}
 
-export default NumberPlateScanner
+    return (
+        <View>
+            <Text>NumberPlateScanner</Text>
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({})
+export default NumberPlateScanner;
+
+const styles = StyleSheet.create({});
---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+import Axios from 'axios';
+import { BACKEND_URL } from '@env';
+
+const axios = Axios.create({
+    baseURL: BACKEND_URL,
+    headers: {
+        Accept: 'application/json,application/x-www-form-urlencoded,text/plain,*/*',
+        'Content-Type': 'multipart/form-data',
+    },
+});
+
+export const postData = async (endpoint: string, data: object) => {
+    try {
+        const response = await axios.post(`/${endpoint}`, data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const getData = async (endpoint: string, params?: object | null) => {
+    try {
+        console.log(BACKEND_URL);
+        const response = await axios.get(`/${endpoint}`, { params });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
