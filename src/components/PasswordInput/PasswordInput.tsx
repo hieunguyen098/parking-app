ddb92d3942892e3d377c7a28685155d1ebca0a27
Binh Cao <binh17122001@gmail.com>
2023-02-13 22:34:19 +0700

    fix mockapi

--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -7,11 +7,16 @@ import styles, { ACTIVE_CELL_BG_COLOR, DEFAULT_CELL_BG_COLOR, NOT_EMPTY_CELL_BG_
 
 const { Value, Text: AnimatedText } = Animated;
 
-const PasswordInput = ({ length }: any) => {
+interface PasswordInput {
+    length: number;
+    value: string;
+    setValue: any;
+}
+
+const PasswordInput = ({ length, value, setValue }: PasswordInput) => {
     const CELL_COUNT = length;
     const animationsColor = [...new Array(CELL_COUNT)].map(() => new Value(0));
     const animationsScale = [...new Array(CELL_COUNT)].map(() => new Value(1));
-    const [value, setValue] = useState('');
     const [target, setTarget] = useState(false);
 
     const onTarget = (value: boolean) => {
@@ -30,7 +35,6 @@ const PasswordInput = ({ length }: any) => {
                       outputRange: [DEFAULT_CELL_BG_COLOR, ACTIVE_CELL_BG_COLOR],
                   }),
         };
-
         return <AnimatedText key={index} style={[styles.cell, animatedCellStyle]} />;
     };
 
--- src/screens/Authentication/Login/Login.tsx
+++ src/screens/Authentication/Login/Login.tsx
@@ -1,29 +1,35 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import PasswordInput from '../../../components/PasswordInput/PasswordInput';
 import LargeButton from '../../../components/Buttons/LargeButton';
 import { useNavigation } from '@react-navigation/native';
 import styles from '../styles';
+import { useSelector } from 'react-redux';
+import { login } from '../../../services/auth';
 
 const Login = () => {
     const navigation: any = useNavigation();
-    const handleFunction = () => {
-        console.log('From login');
-    };
+    const user = useSelector((state: any) => state.auth.user);
+    const [pin, setPin] = useState('');
     const navigate = (name: string) => {
-        navigation.navigate(name, {nextScreen: 'TabBarScreen', action:'login'});
+        navigation.navigate(name, { nextScreen: 'TabBarScreen', action: 'login' });
+    };
+    const onContinue = async () => {
+        const res = await login(user.phone, pin);
+        console.log(user)
+        console.log(res);
     };
     return (
         <View style={styles.container}>
             <View style={styles.group}>
-                <Text style={styles.description}>Xin chào, CAO THANH BÌNH!</Text>
-                <Text style={styles.description}>0398888888</Text>
-                <PasswordInput length={6} />
+                <Text style={styles.description}>Xin chào, {user.fullname}!</Text>
+                <Text style={styles.description}>{user.phone}</Text>
+                <PasswordInput length={6} value={pin} setValue={setPin} />
                 <Pressable onPress={() => navigate('ForgotPassword')}>
                     <Text style={styles.forgotPasswordBtn}>Quên mật khẩu?</Text>
                 </Pressable>
             </View>
-            <LargeButton onPress={() => navigate('PhoneVerification')} title="Tiếp tục" style={styles.continueButton} />
+            <LargeButton onPress={onContinue} title="Tiếp tục" style={styles.continueButton} />
         </View>
     );
 };
